905541341a96ccd558ec2232bb94ac5a36134ea3
w.glanzer <w.glanzer@conceptive.io>
2023-08-06 10:37:23 +0200

    chore: upgraded whole build system to align to "https://github.com/JetBrains/intellij-platform-plugin-template"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,5 @@
-rootProject.name = "quarkus-intellij-plugin"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+}
 
+rootProject.name = "quarkus-intellij-plugin"
